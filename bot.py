0e5b735174793ee5acedcdbf10425dd7022e6eb3
ilyanosovsky <imnosovsky@gmail.com>
2023-09-08 15:35:13 +0300

    draft email func

--- bot.py
+++ bot.py
@@ -1,27 +1,92 @@
-import slack
 import os
-from pathlib import Path
-from dotenv import load_dotenv
-from flask import Flask
-from slackeventsapi import SlackEventAdapter
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_bolt import App
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, request
+from functions import draft_email
 
-env_path = Path('.')/'.env'
-load_dotenv(dotenv_path=env_path)
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
 
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events', app)
+# Set Slack API credentials
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-BOT_ID = client.api_call('auth.test')['user_id']
+# Initialize the Slack app
+app = App(token=SLACK_BOT_TOKEN)
 
-@slack_event_adapter.on('message')
-def message(payload):
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-    if BOT_ID != user_id:
-        client.chat_postMessage(channel=channel_id, text=text)
+# Initialize the Flask app
+# Flask is a web application framework written in Python
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
 
+
+def get_bot_user_id():
+    """
+    Get the bot user ID using the Slack API.
+    Returns:
+        str: The bot user ID.
+    """
+    try:
+        # Initialize the Slack client with your bot token
+        slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+        response = slack_client.auth_test()
+        return response["user_id"]
+    except SlackApiError as e:
+        print(f"Error: {e}")
+
+
+def my_function(text):
+    """
+    Custom function to process the text and return a response.
+    In this example, the function converts the input text to uppercase.
+
+    Args:
+        text (str): The input text to process.
+
+    Returns:
+        str: The processed text.
+    """
+    response = text.upper()
+    return response
+
+
+@app.event("app_mention")
+def handle_mentions(body, say):
+    """
+    Event listener for mentions in Slack.
+    When the bot is mentioned, this function processes the text and sends a response.
+
+    Args:
+        body (dict): The event data received from Slack.
+        say (callable): A function for sending a response to the channel.
+    """
+    text = body["event"]["text"]
+
+    mention = f"<@{SLACK_BOT_USER_ID}>"
+    text = text.replace(mention, "").strip()
+
+    say("Sure, I'll get right on that!")
+    # response = my_function(text)
+    response = draft_email(text)
+    say(response)
+
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    """
+    Route for handling Slack events.
+    This function passes the incoming HTTP request to the SlackRequestHandler for processing.
+
+    Returns:
+        Response: The result of handling the request.
+    """
+    return handler.handle(request)
+
+
+# Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    flask_app.run(debug=True, port=8000)
